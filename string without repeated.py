15950ae0a739e0ba3d84a7df54116fc229ecfe86
giridhararao <www.giridhararao@gmail.com>
2018-07-12 00:06:08 +0530

    Update string without repeated.py

--- string without repeated.py
+++ string without repeated.py
@@ -3,7 +3,8 @@ b=[]
 c=[]
 for j in range(0,len(i)):
       b.append(i[j])
+e=[]
 for j in b:
-     if j not in c:
-           c.append(j)
-print("".join(c))
+     if j not in e:
+           e.append(j)
+print("".join(e))
