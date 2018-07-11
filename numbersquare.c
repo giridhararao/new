0327aa1998f981a0e86d04f12f1b9b7e1f644455
giridhararao <www.giridhararao@gmail.com>
2018-07-11 19:32:19 +0530

    Create numbersquare.c

---
+++ numbersquare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b[100],i=0,j=0,k;
+scanf("%d",&a);
+while(a>0)
+{
+b[i]=a%10;
+a=a/10;
+i=i++;
+}
+for(k=0;k<i;k++)
+{
+j=j+(b[k]*b[k]);
+}
+printf("%d",j);
+return(0);
+}
