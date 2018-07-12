a0c82392ce359c5b0f2bbecc896890bb8312023d
giridhararao <www.giridhararao@gmail.com>
2018-07-12 21:42:11 +0530

    Update appartment.py

--- appartment.py
+++ appartment.py
@@ -2,7 +2,7 @@ x,y=input().split()
 if(x.isdigit() and y.isdigit()): 
               x=int(x)
               y=int(y)
-              if(x=10 and y=1):
+              if(y==1):
                   print("1 2")
               else:
                  print("1 "+str(x-y))
