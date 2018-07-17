a23a0af78a566306f70c0b43552f309f46807f41
giridhararao <www.giridhararao@gmail.com>
2018-07-17 07:49:36 +0530

    Create pro13.py

---
+++ pro13.py
@@ -0,0 +1,29 @@
+n,q=input().split()
+if n.isdigit() and q.isdigit():
+	n=int(n)
+	q=int(q)
+	w=0
+	a=input().split()
+	for i in range(0,len(a)):
+		if a[i].isdigit():
+			w=w+1
+	if w==len(a):
+		l=[]
+		m=[]
+		for i in range(0,q):
+			p=input().split()
+			l.append(p)
+		for i in range(0,len(l)):
+			r=l[i]
+			s=0
+			if r[0].isdigit() and r[1].isdigit():
+				for i in range(int(r[0])-1,int(r[1])):
+					   m.append(int(a[i]))
+				print(min(m))
+				m=[]
+			else:
+				continue
+	else:
+		print("invalid")
+else:
+	print("Invalid")
