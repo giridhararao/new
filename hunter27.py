f02364319c29764c3644d99e4e60902d21379d86
giridhararao <www.giridhararao@gmail.com>
2018-07-17 22:28:26 +0530

    Update hunter27.py

--- hunter27.py
+++ hunter27.py
@@ -1,12 +1,12 @@
-a=input()
-b=[]
-for i in range(0,len(a)):
-	b.append(a[i])
-c="".join(b[::-1])
-if a!=c:
-	print(a)
-elif a==c:
+n=input()
+k=[]
+for i in range(0,len(n)):
+	k.append(n[i])
+c="".join(k[::-1])
+if n!=c:
+	print(n)
+elif n==c:
 	z=[]
-	for i in range(0,len(a)-1):
-		z.append(b[i])
+	for i in range(0,len(n)-1):
+		z.append(k[i])
 	print("".join(z))
