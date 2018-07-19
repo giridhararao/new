f3965b69571e161c1b065b927072ee6458f1e4b3
giridhararao <www.giridhararao@gmail.com>
2018-07-19 16:13:03 +0530

    Update pro33.py

--- pro33.py
+++ pro33.py
@@ -1,6 +1,7 @@
 a=input()
 b=len(a)
 c=[]
+y=0
 for i in range(0,b):
 	c.append(a[i])
 z=ord(c[0])
@@ -8,7 +9,7 @@ k=[]
 for i in range(0,b):
 	if z<ord(c[i]):
 		y=i
-for i in range(0,b):
-	if i>=y:
-		k.append(c[i])
+		break
+for i in range(y,b):
+        k.append(c[i])
 print("".join(k))
