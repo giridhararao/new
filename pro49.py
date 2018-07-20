325b0449dbd1d673904f149c2b0ab711783a9b18
giridhararao <www.giridhararao@gmail.com>
2018-07-20 07:38:53 +0530

    Update pro49.py

--- pro49.py
+++ pro49.py
@@ -1,8 +1,18 @@
 a=input()
 b=input()
 i=1
-while i<len(b):
-	c=a*i
-	if c==b:
+e=len(a)
+f=len(b)
+if(e>f):
+        g=f
+        h=b
+        j=a
+else:
+        g=e
+        h=a
+        j=b
+while i<g:
+	d=h*i
+	if d==j:
 		print(i)
 	i=i+1
